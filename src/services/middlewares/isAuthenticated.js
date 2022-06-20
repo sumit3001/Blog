ef33f7cd9cd724b4b8c76cf7e930fb5564644ea6
youngmansumit <svsumitvishwakarma@gmail.com>
2022-06-20 13:40:01 +0530

    middlewares

---
+++ src/services/middlewares/isAuthenticated.js
@@ -0,0 +1,22 @@
+import { verifyJWT } from "../../utils";
+
+export const isAuthenticated = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split("")[1];
+    const isVerified = verifyJWT(token);
+    if (!isVerified) return next();
+    else {
+      return res.json({
+        data: {},
+        success: false,
+        message: "Unautherised",
+      });
+    }
+  } catch (error) {
+    return res.json({
+      data: {},
+      success: false,
+      message: error.message,
+    });
+  }
+};
